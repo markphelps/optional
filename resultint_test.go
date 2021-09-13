bd7df7f2fd2b13a56a73b356df198ae7afadfbe2
Alex Collins <alex_collins@intuit.com>
2021-09-13 10:18:04 -0700

    chore: fmt

--- resultint_test.go
+++ resultint_test.go
@@ -2,8 +2,9 @@ package optional
 
 import (
 	"errors"
-	"github.com/stretchr/testify/assert"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func conventionalIntReturn(v int, err error) (int, error) {
